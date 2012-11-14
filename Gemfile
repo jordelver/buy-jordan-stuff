c2dea56d7e3c87ca0555298760c8623ca4315668
Jordan Elver <jordan.elver@gmail.com>
2012-11-14 23:51:47 +0000

    Quick 'n' dirty API shenanigans

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ ruby "1.9.3"
 
 gem 'sinatra'
 gem 'thin'
+gem 'rest-client'
+gem 'json'
 
 group :development do
   gem 'foreman'
--- app.rb
+++ app.rb
@@ -1,7 +1,53 @@
 require 'sinatra'
+require 'rest_client'
+require 'json'
+
+class Pinboard
+
+  API_URL = 'https://api.pinboard.in/v1/posts/all'
+
+  attr_reader :token
+
+  def initialize(options = {})
+    @token = options.fetch(:token) { ENV['PINBOARD_TOKEN'] }
+
+    if token.to_s.empty?
+      raise "Missing Pinboard authentication token"
+    end
+  end
+
+  def all(options = {})
+    json = RestClient.get(API_URL, { params: request_params(options) })
+
+    JSON.parse(json).map do |bookmark|
+      Bookmark.new(bookmark["description"], bookmark['href'], bookmark['time'])
+    end
+  end
+
+  def request_params(options)
+    default_params.merge!(options)
+  end
+
+  def default_params
+    {
+      format: 'json',
+      auth_token: token
+    }
+  end
+end
+
+Bookmark = Struct.new(:description, :href, :time)
+
+class Wishlist
+  def items
+    Pinboard.new.all(tag: 'wishlist')
+  end
+end
 
 class BuyJordanStuff < Sinatra::Base
   get '/' do
+    @wishlist = Wishlist.new.items
+
     erb :index
   end
 end
